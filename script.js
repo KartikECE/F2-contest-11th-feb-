e94beb4131ab25497a1393d1d1907dcd4a7e23ac
KartikECE <117670174+KartikECE@users.noreply.github.com>
2023-02-11 22:55:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((students)=>{
+      if(students.marks > 50)
+        console.log(students)
+    });    
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((students) =>{
+        if(students.marks > 50){
+          console.log(students);
+        }
+      });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+    arr.push(newStudent);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i].marks < 50) {
+        arr.splice(i, i);
+      }
+    }
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let students = [
+      { id: 11, name: "Shivangi", age: "24", marks: 78 },
+      { id: 22, name: "Omkar", age: "22", marks: 79 },
+      { id: 33, name: "Nishi", age: "23", marks: 91 },
+    ];
+  
+    console.log(arr.concat(students));
 }
